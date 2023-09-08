7c0bd726c7ad37db37266cb24cffe43cd6703a82
최한슬 <hanseulo@naver.com>
2023-09-09 05:41:13 +0900

    북스탁

--- MMPP/src/Footer.js
+++ MMPP/src/Footer.js
@@ -14,7 +14,7 @@ function Footer() {
       </div>
 
       <div id="companyInfo">
-        <p>북스탁   사업자등록번호:514-81-28277 대표번호:1900-2896</p><br/>
+        <p>북스탁   사업자등록번호:514-81-28277 대표번호:1900-2896</p><br />
         <span>COPYRIGHT(C) {new Date().getFullYear()} 북스탁 RIGHTS RESERVED</span>
       </div>
     </footer>
